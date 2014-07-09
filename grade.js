d102387c6ac342e7b38b4a487fef7b1d825f4683
cameronjacoby <cameron.jacoby@gmail.com>
2014-07-08 22:31:52 -0700

    create grade.js

---
+++ grade.js
@@ -0,0 +1,51 @@
+function assignGrade(score) {
+	if(score >= 97 && score <= 100) {
+		return 'A+';
+	}
+	else if(score >= 93 && score < 97) {
+		return 'A';
+	}
+	else if(score >= 90 && score < 93) {
+		return 'A-';
+	}
+	else if (score >= 87 && score < 90) {
+		return 'B+';
+	}
+	else if (score >= 83 && score < 87) {
+		return 'B';
+	}
+	else if (score >= 80 && score < 83) {
+		return 'B-';
+	}
+	else if (score >= 77 && score < 80) {
+		return 'C+';
+	}
+	else if (score >= 73 && score < 77) {
+		return 'C';
+	}
+	else if (score >= 70 && score < 73) {
+		return 'C-';
+	}
+	else if (score >= 67 && score < 70) {
+		return 'D+';
+	}
+	else if (score >= 63 && score < 67) {
+		return 'D';
+	}
+	else if (score >= 60 && score < 63) {
+		return 'D-';
+	}
+	else if (score >= 0 && score < 60) {
+		return 'F';
+	}
+	else {
+		return 'not valid';
+	}
+}
+
+// testing the function
+console.log('Your grade: ' + assignGrade(73));
+console.log('Your grade: ' + assignGrade(100));
+console.log('Your grade: ' + assignGrade(105)); // should return 'not valid'
+console.log('Your grade: ' + assignGrade(0));
+console.log('Your grade: ' + assignGrade(-28)); // should return 'not valid'
